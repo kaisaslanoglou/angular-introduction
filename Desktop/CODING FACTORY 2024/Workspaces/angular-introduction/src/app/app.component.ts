5fb3d41806df639897acb78cf49fc38a4e47a93f
unknown <k_aslanoglou@hotmail.com>
2024-03-30 20:20:38 +0200

    one-way-binding

--- Desktop/CODING FACTORY 2024/Workspaces/angular-introduction/src/app/app.component.ts
+++ Desktop/CODING FACTORY 2024/Workspaces/angular-introduction/src/app/app.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Kaisariani';
+
+  person = {
+    givenName: 'Kaisariani',
+    surName: 'Aslanoglou',
+    age: 33,
+    email: 'kaslanoglou@aueb.gr'
+  }
 }
