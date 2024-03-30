4b07c4df28b8f861635268d3343235e18706e6d7
unknown <k_aslanoglou@hotmail.com>
2024-03-30 21:48:51 +0200

    component-input

--- Desktop/CODING FACTORY 2024/Workspaces/angular-introduction/src/app/app.component.ts
+++ Desktop/CODING FACTORY 2024/Workspaces/angular-introduction/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
+import { Person } from './shared/interfaces/person';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,20 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 })
 export class AppComponent {
   name = 'Kaisariani';
+  person0: Person =
+    {
+    givenName: 'Kaisariani',
+    surName: 'Aslanoglou',
+    age: 33,
+    email: 'kaslanoglou@aueb.gr',
+    address: "Athens, Greece"
+  }
+  person1: Person =
+    {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 0x25,
+    email: 'john@aueb.gr',
+    address: "New York"
+  }
 }
